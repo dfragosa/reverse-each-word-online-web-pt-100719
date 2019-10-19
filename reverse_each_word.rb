0754693509eed9265f67e2974b822690b588d9a6
Derric Fragosa <fderric@gmail.com>
2019-10-19 15:12:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
+
+def reverse_each_word(string)
+  string =   string.split(",")
+  string.collect do |reverses|
+   puts reverses.reverse
+  end 
+end 
+reverse_each_word("Derric will be the godlike Programmer!")
+
 def reverse_each_word(string)
   string =   string.split(",")
   string.collect do |reverses|
