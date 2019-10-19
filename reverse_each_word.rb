451bd10400e9c2265c25752fa31174e4a380b02a
Derric Fragosa <fderric@gmail.com>
2019-10-19 15:13:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,12 +7,12 @@ def reverse_each_word(string)
 end 
 reverse_each_word("Derric will be the godlike Programmer!")
 
-def reverse_each_word(string)
-  string =   string.split(",")
-  string.collect do |reverses|
-   puts reverses.reverse
-  end 
-end 
-reverse_each_word("Derric will be the godlike Programmer!")
+# def reverse_each_word(string)
+#   string =   string.split(",")
+#   string.collect do |reverses|
+#   puts reverses.reverse
+#   end 
+# end 
+# reverse_each_word("Derric will be the godlike Programmer!")
 
-## turn string to array to_a then use the enumerable
+# ## turn string to array to_a then use the enumerable
